75684d48e82bf9611fcb895133b32f72bbd29881
anewby03 <156264456+anewby03@users.noreply.github.com>
2024-01-23 11:02:40 -0500

    Add files via upload

---
+++ reveiw.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include "review.h"
+
+using namespace std;
+
+void ReadStdIn(){
+
+int integer;
+double dub;
+string string;
+
+std::cin >> integer >> dub >> string;
+
+cout << integer << endl;                             
+    cout << dub << endl; 
+    cout << string << endl;                         
+}
---
+++ testerMain.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include "review.h"
+
+int main(){
+
+ReadStdIn();
+return 0;
+}
